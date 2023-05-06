a8aeda7957d7e282ed278a5a558a14730f6f23cc
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-06 21:22:15 +0530

    4

---
+++ bridge-new/src/Workshop.java
@@ -0,0 +1,15 @@
+public abstract class Workshop {
+    protected Engine engine;
+
+    public Workshop(Engine engine) {
+        this.engine = engine;
+    }
+    abstract void setType();
+    abstract void setSpeed();
+
+    public void process(){
+        setType();
+        setSpeed();
+        engine.assemble();
+    }
+}
