56d8e27123463dcec37cfac5102b75856c4700eb
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-06 21:08:35 +0530

    2

---
+++ src/JapanBmw.java
@@ -0,0 +1,7 @@
+public class JapanBmw extends Engine{
+    @Override
+    public void assemble() {
+        System.out.println("Japan bmw type" + type);
+        System.out.println("Japan bmw speed" + speed);
+    }
+}
---
+++ src/USAAudi.java
@@ -0,0 +1,7 @@
+public class USAAudi extends Engine{
+    @Override
+    public void assemble() {
+        System.out.println("USA Audi type" + type);
+        System.out.println("USA Audi speed" + speed);
+    }
+}
