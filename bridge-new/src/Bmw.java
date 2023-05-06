2f4d2c0e945c9ce4a1226803bf2a010b7f667cfe
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-06 21:15:04 +0530

    3

---
+++ bridge-new/src/Bmw.java
@@ -0,0 +1,7 @@
+public class Bmw extends Engine{
+    @Override
+    public void assemble() {
+        System.out.println("BMW type" + type);
+        System.out.println("BMW speed" + speed);
+    }
+}
